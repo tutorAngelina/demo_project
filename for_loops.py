0e1fa58b66b98a704d1988beed66279f7c443f91
linaguz020 <linaguz020@gmail.com>
2025-05-06 17:11:24 -0700

    created the for_loop file including brief intro

---
+++ for_loops.py
@@ -0,0 +1,11 @@
+"""
+In this file, we will practice using for loops to access elements in arrays and strings.
+
+Functions used:
+range(start, stop, step=1) -> defines a range of values, not including the stop value
+len(s) -> retrieves the length of a string
+
+For loop syntax:
+for _____ in _____:
+    do something
+"""
