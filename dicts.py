75a5be6ef9c3baa9f18d38eba8ebd1f6b4f0bcd2
linaguz020 <linaguz020@gmail.com>
2025-05-14 17:11:07 -0700

    added dicts.py and comments about dictionaries

---
+++ dicts.py
@@ -0,0 +1,14 @@
+'''
+In this file, we will learn about Python dictionaries. Dictionaries are used to store data values in key:value pairs.
+A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+
+Example structure:
+
+california_slang = {
+    "Yeah no" : "No",
+    "No yeah" : "Yes",
+    "Yeah no, for sure" : "Yes"
+}
+
+print(california_slang["No yeah"]) <- this will print "Yes"
+'''
