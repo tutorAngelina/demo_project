baee9fc688ea71192e2b3f0fea182ceea5f36a1b
Siddh Mitesh Doshi <SiddhDoshi@Siddhs-MacBook-Air.local>
2025-05-18 16:10:55 -0700

    Prints names of companies and their founders

---
+++ hw2.py
@@ -0,0 +1,8 @@
+companies = {
+    "Tesla": "Elon Musk",
+    "Facebook": "Mark Zuckerberg",
+    "Amazon": "Jeff Bezos",
+}
+
+for key in companies:
+        print(key, "was founded by", companies[key], end=" ")
